cdc5140d05a908d5c780a8f68e817e3ff6132697
fangchaoqun <fang.chaoqun@nissei-china.cn>
2021-12-16 09:36:31 +0800

    feat: css-in-js

---
+++ pages/posts/first-post.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Head from "next/head";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 20px;
+  color: ${({ theme }) => theme.color};
+`;
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <div>
+        <h1>First Post</h1>
+        <Title theme={{ color: "red" }}>style components</Title>
+        <div className="text-3xl font-bold underline">tailwind css</div>
+        <h2>
+          <Link href="/">
+            <a> Back to home </a>
+          </Link>
+        </h2>
+        <style jsx>{`
+          h2 {
+            color: blue;
+          }
+          h2:hover {
+            text-decoration: underline;
+          }
+        `}</style>
+      </div>
+    </>
+  );
+}
