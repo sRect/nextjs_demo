cd16a7598e05f712e9cff298a459646c4de0e2d8
fangchaoqun <fang.chaoqun@nissei-china.cn>
2021-12-17 16:24:06 +0800

    feat: 自定义404页面

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { Result, Button } from "antd";
+
+export default function Custom404() {
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link href="/">
+          <a>
+            <Button type="link">Back Home</Button>
+          </a>
+        </Link>
+      }
+    />
+  );
+}
